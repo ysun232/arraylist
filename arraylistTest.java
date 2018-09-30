cf5dc1c1ce55bd52deb68142dd5469ed67a96752
SUN YILONG <YSUN232@HOTMAIL.COM>
2018-09-29 22:17:05 -0400

    added a size 3 arraylist

--- arraylistTest.java
+++ arraylistTest.java
@@ -1,7 +1,20 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class arraylistTest{
-    public static void main(String[] args){
-        
-    } 
+
+public class arrayList {
+	public static void main(String[] args){
+		//initiating a new array list as part of the test
+		List<String> aa= new ArrayList<String>();
+		aa.add("This is an arrayList");
+		aa.add("It is more powerful than an array when you need to continuously add elements to a list");
+		aa.add("However you will need to export the elements to an array if you want to call them in your system");
+		
+		
+		//converting the arraylist into an array and printing every element of the array out with a universal for loop
+		String[] abo = aa.toArray(new String[aa.size()]);
+		for(int i=0;i<aa.size();i++){
+			System.out.println(abo[i]);
+		}
+	}
 }
